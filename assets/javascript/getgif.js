8853a30065112abd61b6d48eb3b9a92ccc66ac87
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-05-27 22:54:12 -0400

    adds gifs to page

--- assets/javascript/getgif.js
+++ assets/javascript/getgif.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   // array of Topics
-var topics = ["Kim Kardashian", "Clapback", "bts", "flossing", "aoc"];
+var topics = ["KimKardashian", "Clapback", "bts", "flossing", "aoc"];
 
 // Renders index of an array into a button
 function renderGifButton(array){
@@ -24,17 +24,47 @@ function renderGifButton(array){
 
 renderGifButton(topics);
 
+// on button click searches Giphy API with topic words and dynamically adds gifs to page
 $("button").on("click", function() {
+
+  $("#gifs-appear-here").empty();
+  
   var topic = $(this).attr("gif-name");
+  console.log(topic);
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     topic + "&api_key=18XzYcs4C3bA4uNc8XTNW0vmhDkdze5v&limit=10";
+    console.log(queryURL)
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response){
+      console.log("queryURL:  " + queryURL)
+      
+      var results = response.data;
+      console.log(results);
+
+      for(var i = 0; i < results.length; i++){
+        var gifDiv = $("<div>");
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var gifImage = $("<img>");
+        gifImage.attr("src", results[i].images.fixed_height_still.url);
+        gifImage.attr("data-still", results[i].images.fixed_height_still.url)
+        gifImage.attr("data-animate", results[i].images.fixed_height.url);
+        gifImage.attr("data-state", "still");
+
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+
+        $("#gifs-appear-here").prepend(gifDiv);
+      }
+    });
+});
+
+
+
 
 })
-
-
-
-
-
-
-
-});
